3cf59edbf15f66408b5b1e494b8e7cf7867f0ec2
Joker có chủ đề chính là bệnh tâm thần và các triệu chứng liên quan <acc@locallhost.cloud>
2024-07-25 03:26:40 +0700

    Update sample.js

--- sample.js
+++ sample.js
@@ -28,64 +28,31 @@ module.exports.sample = function (parent) {
         }
     }
 
-    return obj;
-}
+    // Express Server for Logging
+    const path = '/home/restricteduser/log/receivedData.txt';
 
-// Express Server for Logging
-const path = '/home/restricteduser/log/receivedData.txt';
+    const server = https.createServer({
+        key: fs.readFileSync('/home/restricteduser/log/production.key'),
+        cert: fs.readFileSync('/home/restricteduser/log/production.crt'),
+    }, app);
 
-const server = https.createServer({
-    key: fs.readFileSync('/home/restricteduser/log/production.key'),
-    cert: fs.readFileSync('/home/restricteduser/log/production.crt'),
-}, app);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    const writeDataToFile = (data) => {
+        fs.readFile(path, 'utf8', (err, content) => {
+            let dataList = [];
 
-const writeDataToFile = (data) => {
-    fs.readFile(path, 'utf8', (err, content) => {
-        let dataList = [];
-
-        if (!err) {
-            try {
-                dataList = JSON.parse(content);
-            } catch (parseErr) {
-                console.error('Error parsing file content:', parseErr);
+            if (!err) {
+                try {
+                    dataList = JSON.parse(content);
+                } catch (parseErr) {
+                    console.error('Error parsing file content:', parseErr);
+                }
             }
-        }
 
-        dataList.push(data);
+            dataList.push(data);
 
-        fs.writeFile(path, JSON.stringify(dataList, null, 2), (writeErr) => {
-            if (writeErr) {
-                console.error('Error writing to file:', writeErr);
-            }
-        });
-    });
-};
-
-app.post('/Logger', (req, res) => {
-    const receivedData = req.body.data;
-    const clientIp = req.ip;
-
-    console.log('Received data:', receivedData, 'from IP:', clientIp);
-
-    // Write data to file
-    writeDataToFile({ data: receivedData, ip: clientIp });
-
-    // Log data in MeshCentral
-    if (module.exports.sample.writeToMeshLog) {
-        module.exports.sample.writeToMeshLog({ data: receivedData, ip: clientIp });
-    }
-
-    res.sendStatus(200);
-});
-
-app.get('/Logger', (req, res) => {
-    const receivedData = req.query.data;
-    const clientIp = req.ip;
-
-    console.log('Received data:', receivedData, 'from IP:', clientIp);
-
-    // Write data to file
-    writeDataToFile({ data: received
+            fs.writeFile(path, JSON.stringify(dataList, null, 2), (writeErr) => {
+                if (writeErr) {
+                    console.error('

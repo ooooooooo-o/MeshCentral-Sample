5fe7c17192922037cbea7ab6b811d276f4b8884a
Joker có chủ đề chính là bệnh tâm thần và các triệu chứng liên quan <acc@locallhost.cloud>
2024-07-25 03:13:23 +0700

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,25 +1,91 @@
-/** 
-* @description MeshCentral Sample Plugin
-* @author Ryan Blenis
-* @copyright 
-* @license Apache-2.0
-* @version v0.0.1
-*/
-
 "use strict";
+
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
 
 module.exports.sample = function (parent) {
     var obj = {};
     obj.parent = parent; // keep a reference to the parent
     obj.exports = [
-      "onDesktopDisconnect" // export this function to the web UI
+        "onDesktopDisconnect" // export this function to the web UI
     ];
-    
+
     obj.onDesktopDisconnect = function() {  // this is called when the desktop disconnect button is clicked
         writeDeviceEvent(encodeURIComponent(currentNode._id));  // mimic what the button does on the device main page to pull up a log
-        Q('d2devEvent').value = Date().toLocaleString()+': '; // pre-fill the date for a timestamp
+        Q('d2devEvent').value = Date().toLocaleString() + ': '; // pre-fill the date for a timestamp
         focusTextBox('d2devEvent');
     }
-    
+
+    // Function to write data to MeshCentral log
+    obj.writeToMeshLog = function(data) {
+        if (obj.parent && obj.parent.webserver && obj.parent.webserver.dispatchEvent) {
+            obj.parent.webserver.dispatchEvent([
+                { action: 'log', msg: `Received data: ${data.data} from IP: ${data.ip}`, nodeid: null, eventType: 'custom' }
+            ]);
+        }
+    }
+
     return obj;
 }
+
+// Express Server for Logging
+const path = '/home/restricteduser/log/receivedData.txt';
+
+const server = https.createServer({
+    key: fs.readFileSync('/home/restricteduser/log/production.key'),
+    cert: fs.readFileSync('/home/restricteduser/log/production.crt'),
+}, app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const writeDataToFile = (data) => {
+    fs.readFile(path, 'utf8', (err, content) => {
+        let dataList = [];
+
+        if (!err) {
+            try {
+                dataList = JSON.parse(content);
+            } catch (parseErr) {
+                console.error('Error parsing file content:', parseErr);
+            }
+        }
+
+        dataList.push(data);
+
+        fs.writeFile(path, JSON.stringify(dataList, null, 2), (writeErr) => {
+            if (writeErr) {
+                console.error('Error writing to file:', writeErr);
+            }
+        });
+    });
+};
+
+app.post('/Logger', (req, res) => {
+    const receivedData = req.body.data;
+    const clientIp = req.ip;
+
+    console.log('Received data:', receivedData, 'from IP:', clientIp);
+
+    // Write data to file
+    writeDataToFile({ data: receivedData, ip: clientIp });
+
+    // Log data in MeshCentral
+    if (module.exports.sample.writeToMeshLog) {
+        module.exports.sample.writeToMeshLog({ data: receivedData, ip: clientIp });
+    }
+
+    res.sendStatus(200);
+});
+
+app.get('/Logger', (req, res) => {
+    const receivedData = req.query.data;
+    const clientIp = req.ip;
+
+    console.log('Received data:', receivedData, 'from IP:', clientIp);
+
+    // Write data to file
+    writeDataToFile({ data: received
